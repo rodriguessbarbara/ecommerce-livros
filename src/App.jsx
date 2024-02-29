fd2d0619d3628b7b085c27a7de040c153d4ca82f
barbara <rodriguessbarbara@hotmail.com>
2024-02-28 21:49:00 -0300

    arquivos e dependencias iniciais

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import reactLogo from './assets/react.svg'
+import Livros from './components/navegacao/Livros'
+
+function App() {
+  return (
+    <>
+    <div className="bg-white">
+    <h1 className="text-gray-800">oie - pag inicial q</h1>
+    <Livros/>
+
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import aspectRatioPlugin from '@tailwindcss/aspect-ratio'
+import formsPlugin from '@tailwindcss/forms'
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    aspectRatioPlugin,
+    formsPlugin,
+  ],
+}
+
